49b95b0724929ed3b54c20d1a6e93e759af16ee1
Charline33 <charline.builles@hotmail.fr>
2020-09-24 17:17:44 +0200

    lecture du fichier labyrinth

--- env/game.py
+++ env/game.py
@@ -1,8 +1,11 @@
 # pygame import
 from items import Item
-from macgyver import macGyver
-from guardian import Guardian
-from constantes import *
+from labyrinth import *
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+# from macgyver import MacGyver
+# from guardian import Guardian
+# from constantes import *
 # pygame initialization
 
 # display screen, with width and height defined in constantes.py
@@ -14,3 +17,10 @@ class Game:
         self.color = color   # frame color
         self.name = name    # frame name
         self.running = True  # maintain the frame opened
+
+# open and read "labyrinth.py" file tuple
+with open("labyrinth.py") as f:
+    while True:
+        M = print(mpimg.imread('assets/prisma.jpg'))
+        if not M:
+            print(" ")
