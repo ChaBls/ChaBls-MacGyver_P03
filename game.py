f01f87b299bbfb0e6f57d94736c3fa8dd8b0a044
Charline33 <charline.builles@hotmail.fr>
2020-10-01 09:14:19 +0200

    transformation import fichier txt en liste de tuples

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*- 
 # pygame import
 from items import Item
-from labyrinth import *
 # from macgyver import MacGyver
 # from guardian import Guardian
 # from constantes import *
@@ -16,7 +17,16 @@ class Game:
         self.name = name
         self.running = True
 
-# open and read "labyrinth.py" file
-fic = open("labyrinth.py", "r")
-print(lab)
-fic.close()
+i = 0
+# open and read "labyrinth.txt" file as "f"
+with open("labyrinth.txt", "r") as f:
+    my_list = [tuple(map(str, i.split(','))) for i in f]   # split each line with ","
+    # create a list of iterable objects with map() method, from the open file
+    for line in f:
+        for i in line:
+            if i == 'M':
+                print("assets/prisma.jpg")
+                continue
+        pass
+    i += 1
+print(f)
