15092c5ae29faf6133dabf7c56e02698b5e573cc
Charline33 <charline.builles@hotmail.fr>
2020-10-01 09:13:27 +0200

    n'est plus une classe

--- constantes.py
+++ constantes.py
@@ -1,8 +1,6 @@
-class Constante:
-    def __init__():
-        tile_size = 40
-        tile_number = 134
-        row_number = 15
-        column_number = 15
-        width = tile_size * tile_number
-        height = tile_size * tile_number
+tile_size = 40
+tile_number = 134
+row_number = 15
+column_number = 15
+width = tile_size * tile_number
+height = tile_size * tile_number
