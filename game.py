37ee6068d359b9967fa3dee5c8af6ec98d455211
Charline33 <charline.builles@hotmail.fr>
2020-10-09 18:23:22 +0200

    ajout coordonnées + création objets Guardian et Player

--- game.py
+++ game.py
@@ -2,9 +2,8 @@
 # -*- coding:Utf8 -*-
 # pygame import
 from items import Item
-# from macgyver import MacGyver
-# from guardian import Guardian
-# from constantes import *
+from macgyver import MacGyver
+from guardian import Guardian
 # pygame initialization
 
 # display screen, with width and height defined in constantes.py
@@ -19,20 +18,19 @@ class Game:
 
 line = 0
 walls = []
-wall = walls.append
 
 with open("labyrinth.txt", "r") as f:
     for column in range(14):
         if f[column] is M:
-            wall((%line, column))
+            walls.append((%line, column))
         elif f[column] is G:
-            G = guardian
+            guardian =  Guardian(name="Mr Turner", image="assets/Gardien.png", type="your enemy", x=14, y=1)
         elif f[column] is P:
-            P = player
+            player = MacGyver(name="Mac Gyver", type="heroe", image="assets/MacGyver.png", velocity=2, score=str, over=False, x=1, y=1)
 
     for i in range(14):
         for j in range (14):
-            if wall in walls:
+            if element in walls:
                 print('M')
             elif player.rect.x = i and player.rect.y = j:
                 print('P')
