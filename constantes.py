f549ad56ca59844030ce0a0481c8ce2782496064
Charline33 <charline.builles@hotmail.fr>
2020-10-16 20:21:24 +0200

    modif modules

--- constantes.py
+++ constantes.py
@@ -1,6 +1,6 @@
 tile_size = 40
-tile_number = 134
-row_number = 15
-column_number = 15
-width = tile_size * tile_number
-height = tile_size * tile_number
+# tile_number = 134
+# row_number = 15
+# column_number = 15
+# width = tile_size * tile_number
+# height = tile_size * tile_number
