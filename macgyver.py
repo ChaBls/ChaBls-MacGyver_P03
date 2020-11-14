a225f691eade3a322da1782099f8c48c51f99999
Charline33 <charline.builles@hotmail.fr>
2020-11-14 19:46:38 +0100

    creation def direction

--- macgyver.py
+++ macgyver.py
@@ -1,11 +1,40 @@
 # !/usr/bin/env python
-# -*- coding:Utf8 -*- 
+# -*- coding:Utf8 -*-
+from constantes import *
 
 
 class MacGyver:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x=x
         self.y=y
+
+    def direction(self):
+        direc_key = input("R:right, L:left, D:down, U:up")
+        if direc_key == 'R'':
+            if self.x == column_number:
+                print("Access denied")
+            else:
+                self.x+=1
+                return self.x
+        if direc_key == 'L':
+            if self.x == 0:
+                print("Access denied")
+            else:
+                self.x-=1
+                return self.x
+        if direc_key == 'D':
+            if self.y == row_number:
+                print("Access denied")
+            else:
+                self.y+=1
+                return self.y
+        if direc_key == 'U':
+            if selx.y == 0:
+                print("Access denied")
+            else:
+                self.y-=1
+                return self.y
+
 
     def direction_update(self):
         '''For each given keyboard key, a direction will be assigned.'''
