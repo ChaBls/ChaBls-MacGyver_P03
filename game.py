a580b70d38d2e0a03c7ffd7f9b3aae297847ae26
Charline33 <charline.builles@hotmail.fr>
2020-11-14 19:46:13 +0100

    modif import modules

--- game.py
+++ game.py
@@ -18,23 +18,21 @@ class Game:
         for indexLine, line in enumerate(file):
             for indexCharacter, character in enumerate(line):
                 if character == "M":
-                    self.walls.append(indexCharacter, indexLine)
+                    self.walls.append((indexCharacter, indexLine))
                 elif character == "G":
                     self.guardian =  Guardian(type="your ennemy", x=indexCharacter, y=indexLine)
                 elif character == "P":
                     self.player = MacGyver(x=indexCharacter, y=indexLine)
 
     def labyrinth_printing(self):
-        for i in range(14):
-            for j in range(14):
-                if (i, j) in self.walls:
-                    print("M")
-                elif self.player.x == i and self.player.y == j:
-                    print("P")
-                elif self.guardian.x == i and self.guardian.y == j:
-                    print("G")
-                else:
-                    print(" ")
-
-labRead = labyrinth_reading()
-labPrint = labyrinth_printing()
+        for i in range(14): # For x(=line) from 0 to 14 (create a list)
+            for j in range(14): # For y(=column) from 0 to 14 (create a list)
+                if (i, j) in self.walls:    # If x and y are both in "self.walls" list, print "M"
+                    print("M", end="")
+                elif self.player.x == i and self.player.y == j: # Elif both player x and y correspond to i and j : print "P"
+                    print("P", end="")
+                elif self.guardian.x == i and self.guardian.y == j: # Elif both guardian x and y correspond to i and j : print "G"
+                    print("G", end="")
+                else:   # None of the above : print an empty space
+                    print(" ", end="")
+            print("\n")
