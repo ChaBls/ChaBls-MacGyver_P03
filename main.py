60a94b5f2222d63b99ac7f42028ef5136db83280
Charline33 <charline.builles@hotmail.fr>
2020-11-18 22:16:55 +0100

    modif appel fonctions

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 # !/usr/bin/env python
 # -*- coding:Utf8 -*-
-from game import Game   # Import the "Game" class from "game.py"
+from game import Game
 from macgyver import MacGyver
 import random
 from random import randint
-from constantes import *
 
 
-def main():     # Called if the file is imported as a module
-    labyrinth = Game()
-    labyrinth.labyrinth_reading()
-    labyrinth.labyrinth_printing()
+labyrinth = Game()
 
-
-# Player object
-player=MacGyver()
-
-# x and y are randomly generated when the game starts
-player.x=random.randint(0,14)
-player.y=random.randint(0,14)
-
-running=True
-
-while running:
-    player.direction()
-
-
-if __name__ == "__main__":  # Call "main()" if we run the file directly (not as a module)
-    main()
+def main(self):
+    # Player position is randomly generated when the game starts
+    self.player.x=random.randint(0,14)
+    self.player.y=random.randint(0,14)
+    running=True
+    while running:
+        labyrinth.lab_reading()
+        labyrinth.lab_printing()
+        self.player.direction()
