02b5ccda0e3522dfe357fcd5fd3e4688ca6b2f35
Charline33 <charline.builles@hotmail.fr>
2020-11-18 22:16:03 +0100

    reectriture du fichier

--- game.py
+++ game.py
@@ -1,38 +1,53 @@
 # !/usr/bin/env python
 # -*- coding:Utf8 -*-
-# pygame import
-from items import Item
 from macgyver import MacGyver
 from guardian import Guardian
-# pygame initialization
-
-# display screen, with width and height defined in constantes.py
-# display player, guardian, items and inventory
 
 
 class Game:
-    def labyrinth_reading(self):
-        self.walls = []
+    def lab_reading(self):
+        self.walls=[]
 
         file = open("labyrinth.txt", "r").readlines()
-        for indexLine, line in enumerate(file):
-            for indexCharacter, character in enumerate(line):
-                if character == "M":
-                    self.walls.append((indexCharacter, indexLine))
-                elif character == "G":
-                    self.guardian =  Guardian(type="your ennemy", x=indexCharacter, y=indexLine)
-                elif character == "P":
-                    self.player = MacGyver(x=indexCharacter, y=indexLine)
+        # Open labyrinth file and save it in the variable 'file'
+        
+        for coorLine, line in enumerate(file):
+        # Get line coordinates (from file), save it in coorLine
 
-    def labyrinth_printing(self):
-        for i in range(14): # For x(=line) from 0 to 14 (create a list)
-            for j in range(14): # For y(=column) from 0 to 14 (create a list)
-                if (i, j) in self.walls:    # If x and y are both in "self.walls" list, print "M"
-                    print("M", end="")
-                elif self.player.x == i and self.player.y == j: # Elif both player x and y correspond to i and j : print "P"
-                    print("P", end="")
-                elif self.guardian.x == i and self.guardian.y == j: # Elif both guardian x and y correspond to i and j : print "G"
-                    print("G", end="")
-                else:   # None of the above : print an empty space
+            for coorLetter, letter in enumerate(line):
+            # Get letter coordinates (from line), save it in coorLetter
+
+                """If the letter corresponds to 'M', we will add its position to walls list
+                If the letter corresponds to 'P' or 'G', we'll create player
+                or guardian objects, with their own position
+                """
+
+                if letter == 'M':
+                    self.walls.append((coorLine, coorLetter))
+                elif letter == 'G':
+                    self.guardian = Guardian(type="ennemy", x=coorLetter, y=coorLine)
+                elif letter == 'P':
+                    self.player = MacGyver(x=coorLetter, y=coorLine)
+
+    def lab_printing(self):
+        
+        """For i position between 0 and 14 and j position between 0 and 14 : compare 
+        these positions to player, guardian and walls coordinates. According to the
+        result, print the appropriate letter (M for walls, P for player or G for
+        guardian
+        """
+
+        for i in range(14):
+            for j in range(14):
+                if (i,j) in self.walls:
+                    print('M', end="")
+                    # Print 'M' and removes all characters/signs placed after it
+
+                elif i == self.player.x and j == self.player.y:
+                    print('P', end="")
+                elif i == self.guardian.x and j == self.guardian.y:
+                    print('G', end="")
+                else:
                     print(" ", end="")
-            print("\n")
+            print("\n")         
+            # New line
