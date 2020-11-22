7cee4c2a8a2bd978203f5e5ed57dce467350dd61
Charline33 <charline.builles@hotmail.fr>
2020-11-22 03:27:03 +0100

    creation objets items

--- main.py
+++ main.py
@@ -2,18 +2,32 @@
 # -*- coding:Utf8 -*-
 from game import Game
 from macgyver import MacGyver
+from items import Item
+from constantes import *
 import random
 from random import randint
 
 
+# Objects
 labyrinth = Game()
 
-def main(self):
-    # Player position is randomly generated when the game starts
-    self.player.x=random.randint(0,14)
-    self.player.y=random.randint(0,14)
-    running=True
-    while running:
-        labyrinth.lab_reading()
-        labyrinth.lab_printing()
-        self.player.direction()
+ether=item_list.append(Item(x=random.randint(0,14),y=random.randint(0,14)))
+needle=item_list.append(Item(x=random.randint(0,14),y=random.randint(0,14)))
+plastic_tube=item_list.append(Item(x=random.randint(0,14),y=random.randint(0,14)))
+
+# Methods
+labyrinth.lab_reading()
+labyrinth.lab_printing(item_list)
+
+# Loop
+running=True
+
+"""The method 'direction()' is called while the game is running.
+The player can now chose MacGyver direction continually.
+Updated labyrinth is printed after each direction change.
+"""
+
+while running:
+    labyrinth.player.direction()
+    labyrinth.lab_printing(item_list)
+    continue
