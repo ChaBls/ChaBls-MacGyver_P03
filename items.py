16507807923f4aa99ad993cf834eca470ba50af4
Charline33 <charline.builles@hotmail.fr>
2020-11-22 03:27:28 +0100

    modification attributs x et y

--- items.py
+++ items.py
@@ -1,17 +1,12 @@
 # !/usr/bin/env python
-# -*- coding:Utf8 -*- 
-import random
+# -*- coding:Utf8 -*-
 
 
 class Item:
-    """ In this class, I created an instance of
-     self as an object, with name, image, x and y as attributes."""
-    def __init__(self, x, y):
+
+    """In this class, x and y are assigned by default
+    """
+    
+    def __init__(self, x=0,y=0):
         self.x=x
         self.y=y
-
-""" New objects are created, from Item. For each of them, I configure their own
-attributes. x and y are randomly generated"""
-ether = Item(x=random.randint(0, 14), y=random.randint(0, 14))  # random integer in range (0, 14)
-needle = Item(x=random.randint(0, 14), y=random.randint(0, 14))
-plastic_tube = Item(x=random.randint(0, 14), y=random.randint(0, 14))
