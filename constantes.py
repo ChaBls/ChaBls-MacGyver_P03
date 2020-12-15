3f6ea974fb92996a4096109d3a9031e1079ffbce
Charline33 <charline.builles@hotmail.fr>
2020-12-15 16:53:15 +0100

    modif imges items

--- constantes.py
+++ constantes.py
@@ -1,7 +1,8 @@
 column_number = 14
 row_number = 14
+
 item_list=[
-    {'name':'ether','image':'E'},
-    {'name':'needle','image':'N'},
-    {'name':'plastic_tube','image':'T'}
+    {'name':'ether','image': "assets/ether.png"},
+    {'name':'needle','image': "assets/aiguille.png"},
+    {'name':'plastic_tube','image': "assets/tube_plastique.png"}
 ]
