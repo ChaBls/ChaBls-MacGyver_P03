638c311ae94b0dda88ee211e9783084d2fcf4080
Charline33 <charline.builles@hotmail.fr>
2020-12-26 19:16:39 +0100

    modif attributs

--- guardian.py
+++ guardian.py
@@ -6,7 +6,6 @@ from items import Item
 class Guardian():
     def __init__(self,x,y,type,name=None,image=None):
         Item.__init__(self,x,y,name=None,image=None)
-        self.x=x
         self.y=y
         self.name=name
         self.image=image
