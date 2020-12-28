ab2db6e753d22ebd1ca7c1a8bf2f34071c89229e
Charline33 <charline.builles@hotmail.fr>
2020-12-28 23:06:51 +0100

    ajout taille et nombre d'images

--- constantes.py
+++ constantes.py
@@ -1,8 +1,14 @@
 column_number = 14
 row_number = 14
+sprite_height = 40
+sprite_width = 40
+display_width = 300
+display_height = 300
 
 item_list=[
     {'name':'ether','image': "assets/ether.png"},
     {'name':'needle','image': "assets/aiguille.png"},
-    {'name':'plastic_tube','image': "assets/tube_plastique.png"}
+    {'name':'plastic tube','image': "assets/tube_plastique.png"},
+    {'name':'swiss knife','image':"assets/couteau_suisse.png"},
+    {'name':'rope','image':"assets/rope.png"}
 ]
