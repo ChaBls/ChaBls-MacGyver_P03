36f245dfebab7a22a6016b9a4db6cebdfbe887e5
Charline33 <charline.builles@hotmail.fr>
2020-12-28 02:18:40 +0100

    essais blit avec Pygame

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from game import Game
 from macgyver import MacGyver
 pygame.init()
 
-
 """The method 'direction()' is called while the game is running.
 The player can now chose MacGyver direction continually.
 Updated labyrinth is printed after each direction change.
@@ -16,25 +15,34 @@ Updated labyrinth is printed after each direction change.
 
 labyrinth = Game()
 
-labyrinth.lab_reading()
+black = (0,0,0)
+white = (255,255,255)
 
-floor = labyrinth.floor
-walls = labyrinth.walls
-item_object = labyrinth.item_object
-guardian = labyrinth.guardian
+pygame.display.set_caption("MACGYVER QUEST")
+screen = pygame.display.set_mode((650,650))
+background = pygame.image.load("assets/black_wallpaper.jpg").convert_alpha()
+screen_size = screen.get_height() * screen.get_width()
+screen.fill(white)
+
+labyrinth.lab_reading()
 
 running=True
 
 while running:
-    labyrinth.lab_printing()
-    labyrinth.player.direction(walls,event.key)
-    labyrinth.player.caught_item(floor,item_object)
-    labyrinth.player.inventory_update()
-    labyrinth.player.guardian_interaction(guardian,floor,item_object)
+    labyrinth.lab_printing(screen,background)
+    screen.blit(background,screen_size)
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.QUIT:
+            quit()
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-                pygame.quit()
+                quit()
             else:
-                labyrinth.player.direction(walls,event.key)
+                labyrinth.player.direction(labyrinth.walls,event.key)
+                labyrinth.player.caught_item(labyrinth.floor,labyrinth.item_object)
+                labyrinth.player.inventory_update()
+                labyrinth.player.guardian_interaction(labyrinth.guardian,labyrinth.floor,labyrinth.item_object)
+    
+    pygame.display.update()
+
