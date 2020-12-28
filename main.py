ece26c86c37ee076dae27cbb82453f9816dd9154
Charline33 <charline.builles@hotmail.fr>
2020-12-28 23:08:00 +0100

    modif boucle et ajout musique

--- main.py
+++ main.py
@@ -1,45 +1,55 @@
 # !/usr/bin/env python
 # -*- coding:Utf8 -*-
 import os
-os.environ['SDL_AUDIODRIVER'] = 'dsp'
 import sys
 import pygame
 from game import Game
 from macgyver import MacGyver
+from constantes import sprite_width
+from constantes import sprite_height
+from constantes import display_width
+from constantes import display_height
+
 pygame.init()
+pygame.mixer.init()
+
 
 """The method 'direction()' is called while the game is running.
 The player can now chose MacGyver direction continually.
 Updated labyrinth is printed after each direction change.
 """
 
-labyrinth = Game()
+labyrinth = Game()  # Create an object of Game class
 
-black = (0,0,0)
-white = (255,255,255)
+screen = pygame.display.set_mode((600,600)) # Initialize the window
+pygame.display.set_caption("MACGYVER QUEST")    # Initialize game title
+screen_rect = screen.get_rect() # Get the rectangular area of the surface
+background = pygame.image.load("assets/black_wallpaper.jpg").convert()  # Create a background with a specific image
 
-pygame.display.set_caption("MACGYVER QUEST")
-screen = pygame.display.set_mode((650,650))
-background = pygame.image.load("assets/black_wallpaper.jpg").convert_alpha()
-screen_size = screen.get_height() * screen.get_width()
-screen.fill(white)
+theme = "assets/MG_theme_song.wav"
 
-labyrinth.lab_reading()
+labyrinth.lab_reading() # Call Game method
 
-running=True
+over = False
 
-while running:
-    labyrinth.lab_printing(screen,background)
-    screen.blit(background,screen_size)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit()
+while not over:
+    screen.blit(background,screen_rect) # Appply background to the screen, with screen surface as a reference for the position
+    labyrinth.lab_printing(background,sprite_width,sprite_height)  # Call Game method
+    pygame.mixer.music.load(theme)
+    pygame.mixer.music.play(-1)
+    for event in pygame.event.get():    # Do something, according to event
+        if event.type == pygame.QUIT:   # If the player clic on window exit cross
+            over = True
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-                quit()
+            event_key = event.key
+            K_LEFT=pygame.K_LEFT
+            K_RIGHT=pygame.K_RIGHT
+            K_DOWN=pygame.K_DOWN
+            K_UP=pygame.K_UP
+            if event_key == pygame.K_ESCAPE:
+                over = True
             else:
-                labyrinth.player.direction(labyrinth.walls,event.key)
+                labyrinth.player.direction(labyrinth.walls,event.key,pygame.K_RIGHT,pygame.K_LEFT,pygame.K_DOWN,pygame.K_UP)
                 labyrinth.player.caught_item(labyrinth.floor,labyrinth.item_object)
                 labyrinth.player.inventory_update()
                 labyrinth.player.guardian_interaction(labyrinth.guardian,labyrinth.floor,labyrinth.item_object)
