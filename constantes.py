3cf3f077b031d19e49af146df6d0523ac784ce4c
Charline33 <charline.builles@hotmail.fr>
2020-12-30 18:45:08 +0100

    ajout constantes theme et yellow

--- constantes.py
+++ constantes.py
@@ -4,11 +4,13 @@ sprite_height = 40
 sprite_width = 40
 display_width = 300
 display_height = 300
+theme = "assets/MG_theme_song.wav"
+yellow = (255,204,51)
 
 item_list=[
     {'name':'ether','image': "assets/ether.png"},
     {'name':'needle','image': "assets/aiguille.png"},
     {'name':'plastic tube','image': "assets/tube_plastique.png"},
     {'name':'swiss knife','image':"assets/couteau_suisse.png"},
-    {'name':'rope','image':"assets/rope.png"}
+    {'name':'mushroom','image':"assets/mushroom.png"}
 ]
