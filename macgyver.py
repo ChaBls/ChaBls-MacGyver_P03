2d149ff544a837358d2ab45a6ed560dc1ad9a20f
Charline33 <charline.builles@hotmail.fr>
2021-01-04 16:50:48 +0100

    suppression texte

--- macgyver.py
+++ macgyver.py
@@ -2,8 +2,6 @@
 # -*- coding:Utf8 -*-
 from constantes import row_number
 from constantes import column_number
-from constantes import coor_main_TextX
-from constantes import coor_main_TextY
 from constantes import white
 from guardian import Guardian
 import game
@@ -25,56 +23,72 @@ class MacGyver:
         self.score_update = 0
 
 
-    def go_right(self,walls):
-        """The 4 following methods determine what "right", "left", "down"
+    def direction(self,walls):
+        """The following methods determine what "right", "left", "down"
         and "up" directions are. Meanwhile, collisions are also managed :
         if player coordinates are matching with walls or border ones,
         player x and y axis return to their previous position.
         """
 
-        self.x+=1
-        if self.x>=column_number:
-            self.x-=1
-            return self.x
-        elif (self.y,self.x) in walls:
-            self.x-=1
-            return self.x
-        else:
-            return self.x
+        # Do something, according to event
+        for event in pygame.event.get():
 
-    def go_left(self,walls):
-        self.x-=1
-        if self.x<=0:
-            self.x+=1
-            return self.x
-        elif (self.y,self.x) in walls:
-            self.x+=1
-            return self.x
-        else:
-            return self.x
+            # If the player press any keyboard key
+            if event.type == pygame.KEYDOWN:
 
-    def go_down(self,walls):        
-        self.y+=1
-        if self.y>=row_number:
-            self.y-=1
-            return self.y
-        elif (self.y,self.x) in walls:
-            self.y-=1
-            return self.y
-        else:
-            return self.y
+                # If the pressed key is right direction arrow
+                if event.key == pygame.K_RIGHT:
+                    self.x+=1
+                    if self.x>=column_number:
+                        self.x-=1
+                        return self.x
+                    elif (self.y,self.x) in walls:
+                        self.x-=1
+                        return self.x
+                    else:
+                        return self.x
 
-    def go_up(self,walls):
-        self.y-=1
-        if self.y<=0:
-            self.y+=1
-        elif (self.y,self.x) in walls:
-            self.y+=1
-            return self.y
-        else:
-            return self.y
+                elif event.key == pygame.K_LEFT:
+                    self.x-=1
+                    if self.x<=0:
+                        self.x+=1
+                        return self.x
+                    elif (self.y,self.x) in walls:
+                        self.x+=1
+                        return self.x
+                    else:
+                        return self.x
 
-    def caught_item(self,floor,item_object,background):
+                elif event.key == pygame.K_DOWN:
+                    self.y+=1
+                    if self.y>=row_number:
+                        self.y-=1
+                        return self.y
+                    elif (self.y,self.x) in walls:
+                        self.y-=1
+                        return self.y
+                    else:
+                        return self.y
+
+                elif event.key == pygame.K_UP:
+                    self.y-=1
+                    if self.y<=0:
+                        self.y+=1
+                    elif (self.y,self.x) in walls:
+                        self.y+=1
+                        return self.y
+                    else:
+                        return self.y
+
+                # The player can close the screen by pressing 'ECHAP'  
+                elif event.key == pygame.K_ESCAPE:
+                    quit()
+
+            # Quit the game, if the player clic on window exit cross
+            elif event.type == pygame.QUIT:
+                quit()
+
+    def caught_item(self,floor,item_object,screen):
         """Method will print sentences everytime MacGyver
         caught an item, and then kills (removes) the corresponding item.
         The player know continuously how many items still have to be grabbed.
@@ -85,25 +99,13 @@ class MacGyver:
             for loot in item_object:
                 if (self.y,self.x) == (loot.y,loot.x):
                     artifact=True
-                    if loot.name == 'swiss knife':
+                    if loot.name == 'mushroom':
                         item_object.remove(loot)
-                        swissKnife_sentence = "Do you really need a {}...?".format(loot.name)
-                        swissKnife_blit = main_font.render((swissKnife_sentence),True,white)
-                        background.blit(swissKnife_blit,(coor_main_TextX,coor_main_TextY))
-                        return (self.y,self.x)
-                    elif loot.name == 'mushroom':
-                        item_object.remove(loot)
-                        mushroom_sentence = "Don't eat this... too late ?!"
-                        mushroom_blit = main_font.render((mushroom_sentence),True,white)
-                        background.blit(mushroom_blit,(coor_main_TextX,coor_main_TextY))
                         return (self.y,self.x)
                     else:
                         self.score_update+=1
                         self.inventory.append(loot)
                         item_object.remove(loot)
-                        loot_sentence = "You caught {}".format(loot.name)
-                        loot_blit = main_font.render((loot_sentence),True,white)
-                        background.blit(loot_blit,(coor_main_TextX,coor_main_TextY))
                         return (self.y,self.x)
             if artifact==False:
                 pass
@@ -116,24 +118,19 @@ class MacGyver:
         a mesage is printed, player wins and the game is over)
         """
 
-        clock = pygame.time.Clock()
-
         ennemy=False
         if (self.y,self.x) == (guardian.y,guardian.x):
             ennemy=True
             if len(self.inventory)==3:
-                winner_sentence="YEEES!! Mac Gyver put his {}".format(guardian.type),"down!\n","YOU WON!"
+                winner_sentence="'YEEES!! Mac Gyver put his ennemy down!\n', 'YOU WON!'"
                 winner = main_font.render((winner_sentence),True,white)
                 background.blit(winner,(coor_main_TextX,coor_main_TextY))
-                pygame.display.update()
-                time.sleep(5)
                 quit()
             else:
                 loser_sentence="You don't have all the items... you lose!"
                 loser = main_font.render((loser_sentence),True,white)
-                background.blit(loser,(coor_main_TextX,coor_main_TextY))
-                pygame.display.update()
-                time.sleep(5)
+                for i in range(1):
+                    background.blit(loser,(coor_main_TextX,coor_main_TextY))
                 quit()
         elif ennemy==False:
             pass
