265a608b3dd12e8813a69da1df2fba1ce5497598
Charline33 <charline.builles@hotmail.fr>
2021-01-04 16:50:12 +0100

    suppression texte

--- constantes.py
+++ constantes.py
@@ -9,16 +9,11 @@ display_height = 300
 coor_score_TextX = 415
 coor_score_TextY = 5
 
-# Main text coordinates
-coor_main_TextX = 200
-coor_main_TextY = 200
-
 white = (255,255,255)
 
 item_list= [
     {'name':'ether','image': "assets/ether.png"},
     {'name':'needle','image': "assets/aiguille.png"},
     {'name':'plastic tube','image': "assets/tube_plastique.png"},
-    {'name':'swiss knife','image':"assets/couteau_suisse.png"},
     {'name':'mushroom','image':"assets/mushroom.png"}
 ]
