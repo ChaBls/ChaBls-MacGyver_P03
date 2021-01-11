6c62c517aca5f01a50ac0af1afc2f527f8ea8524
Charline33 <charline.builles@hotmail.fr>
2021-01-11 16:49:36 +0100

    ajout attribut

--- items.py
+++ items.py
@@ -3,8 +3,9 @@
 
 
 class Item:
-    def __init__(self,x,y,name,image):
+    def __init__(self,x,y,name,image,drug):
         self.x=x
         self.y=y
         self.name=name
         self.image=image
+        self.drug=drug
