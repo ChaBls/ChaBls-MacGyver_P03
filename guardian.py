f16490d2fa5b0ac670a9ade1aec7ca110b0c1328
Charline33 <charline.builles@hotmail.fr>
2021-01-11 16:47:56 +0100

    modif drug

--- guardian.py
+++ guardian.py
@@ -5,7 +5,7 @@ from items import Item
 
 class Guardian():
     def __init__(self,x,y,type,name=None,image=None):
-        Item.__init__(self,x,y,name=None,image=None)
+        Item.__init__(self,x,y,name=None,image=None,drug=None)
         self.name=name
         self.image=image
         self.rect=self.image.get_rect()
