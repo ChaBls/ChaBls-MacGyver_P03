a9fbbfdf254718b8f5faf47ce2fe88bd3893bdbe
Charline33 <charline.builles@hotmail.fr>
2021-01-11 16:46:43 +0100

    ajout coordonnées du texte

--- constantes.py
+++ constantes.py
@@ -9,11 +9,14 @@ display_height = 300
 coor_score_TextX = 390
 coor_score_TextY = 605
 
+coor_main_textX = 20
+coor_main_textY = 612
+
 white = (255,255,255)
 
 item_list= [
-    {'name':'ether','image': "assets/ether.png"},
-    {'name':'needle','image': "assets/aiguille.png"},
-    {'name':'plastic tube','image': "assets/tube_plastique.png"},
-    {'name':'mushroom','image':"assets/mushroom.png"}
+    {'name':'ether','image': "assets/ether.png",'drug':False},
+    {'name':'needle','image': "assets/aiguille.png",'drug':False},
+    {'name':'plastic tube','image': "assets/tube_plastique.png",'drug':False},
+    {'name':'mushroom','image':"assets/mushroom.png",'drug':True}
 ]
