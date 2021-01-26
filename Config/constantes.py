692eba11c9dee4b76cb9f2493a5366a5fe604e6a
Charline33 <charline.builles@hotmail.fr>
2021-01-26 17:32:15 +0100

    New package created

---
+++ Config/constantes.py
@@ -0,0 +1,32 @@
+""" 'colum_number' and 'row_number' are
+used to determine labyrinth borders
+"""
+column_number = 14
+row_number = 14
+
+# Size of each image used into the game (40x40)
+sprite_height = 40
+sprite_width = 40
+
+# score text coordinates
+coor_score_TextX = 390
+coor_score_TextY = 605
+
+# 'Game Over' text coordinates
+coor_main_textX = 20
+coor_main_textY = 612
+
+# Place where 'Game Over' text is displayed
+display_width = 300
+display_height = 300
+
+# Text color
+white = (255,255,255)
+
+# List of 3 dictionnaries, used in 'game.py'
+item_list= [
+    {'name':'ether','image': "Config/assets/ether.png",'drug':False},
+    {'name':'needle','image': "Config/assets/aiguille.png",'drug':False},
+    {'name':'plastic tube','image': "Config/assets/tube_plastique.png",'drug':False},
+    {'name':'mushroom','image':"Config/assets/mushroom.png",'drug':True}
+]
