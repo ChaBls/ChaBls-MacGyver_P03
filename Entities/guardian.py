6c5243fd38f54a07ace27ccf527a8b7f71db3964
Charline33 <charline.builles@hotmail.fr>
2021-01-27 17:39:30 +0100

    import modified

--- Entities/guardian.py
+++ Entities/guardian.py
@@ -4,6 +4,12 @@ from Entities.items import Item
 
 
 class Guardian():
+    """This class is related to Item one.
+    Most of guardian attributes are 'None' by default
+    and are setted in 'lab_reading' ('game' file), when
+    Guardian object is created.
+    """
+    
     def __init__(self,x,y,type,name=None,image=None):
         Item.__init__(self,x,y,name=None,image=None,drug=None)
         self.name=name
