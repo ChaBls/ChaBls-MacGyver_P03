c3b18e301b0a2776827659bb9b32341bb9cf36d8
Charline33 <charline.builles@hotmail.fr>
2021-02-01 16:29:55 +0100

    maj

--- Config/constantes.py
+++ Config/constantes.py
@@ -4,30 +4,26 @@
 """ 'colum_number' and 'row_number' are
 used to determine labyrinth borders
 """
-column_number = 14
-row_number = 14
+COLUMN_NUMBER = 14
+ROW_NUMBER = 14
 
 # Size of each image used into the game (40x40)
-sprite_height = 40
-sprite_width = 40
+SPRITE_HEIGHT = 40
+SPRITE_WIDTH = 40
 
 # score text coordinates
-coor_score_TextX = 390
-coor_score_TextY = 605
+COOR_SCORE_TEXTX = 390
+COOR_SCORE_TEXTY = 605
 
 # 'Game Over' text coordinates
-coor_main_textX = 20
-coor_main_textY = 612
-
-# Place where 'Game Over' text is displayed
-display_width = 300
-display_height = 300
+COOR_MAIN_TEXTX = 20
+COOR_MAIN_TEXTY = 612
 
 # Text color
-white = (255,255,255)
+WHITE = (255,255,255)
 
 # List of 3 dictionnaries, used in 'game.py'
-item_list= [
+ITEM_LIST= [
     {'name':'ether','image': "Entities/assets/ether.png",'drug':False},
     {'name':'needle','image': "Entities/assets/aiguille.png",'drug':False},
     {'name':'plastic tube','image': "Entities/assets/tube_plastique.png",'drug':False},
